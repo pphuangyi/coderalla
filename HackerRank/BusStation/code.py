16f1489c1b6981cf04923307d75fb3dd16388374
Yi Huang <pphuangyi@gmail.com>
2021-04-22 22:48:36 -0500

    Come on mathematician, you are supposed to be detail oriented!

---
+++ HackerRank/BusStation/code.py
@@ -0,0 +1,31 @@
+def helper(arr, k):
+    A = 0
+    for i, a in enumerate(arr):
+        A += a
+        if A == k:
+            A = 0
+        elif A > k:
+            return False
+    return A == 0
+
+def solve(arr):
+    M = sum(arr)
+    prefix_sum = 0
+    results = []
+    for i, a in enumerate(arr):
+        prefix_sum += a
+        if M % prefix_sum != 0:
+            continue
+        if helper(arr[i + 1:], prefix_sum):
+            print(prefix_sum)
+            results.append(prefix_sum)
+    return results
+
+
+if __name__ == '__main__':
+    input_fname = 'data/input07.txt'
+    with open(input_fname, 'r') as handle:
+        n = int(handle.readline().strip())
+        arr = list(map(int, handle.readline().strip().split()))
+    result = solve(arr)
+    print(result)
